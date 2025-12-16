606fb9a2b3a97bf5f2d8e682ca033bf0ffda837b
abstraq <contact@abstraq.dev>
2025-12-16 00:42:58 -0500

    feat(camera): added basic camera controller

--- src/camera.rs
+++ src/camera.rs
@@ -1,9 +1,59 @@
 use bevy::prelude::*;
 
+/// Marker component for the entity used as the primary camera for the game.
+///
+/// Systems for the camera expect this component to only be present on a single
+/// entity.
+#[derive(Component, Debug)]
+pub struct PrimaryCamera;
+
+/// Marker component for the entity that the camera should focus on.
+///
+/// Systems for the camera expect this component to only be present on a single
+/// entity.
+#[derive(Component, Debug)]
+pub struct CameraTarget;
+
+const CAMERA_LERP_FACTOR: f32 = 2.8;
+
+/// This plugin contains systems relating to the camera for the game.
+///
+/// Adding this plugin to the game will handle spawning and interacting with the
+/// primary game camera.
 pub(super) fn plugin(app: &mut App) {
 	app.add_systems(Startup, spawn_camera);
+	app.add_systems(Update, follow_camera_target);
 }
 
+/// Spawns the primary camera entity.
+///
+/// This system is run on the `Startup` schedule and will spawn a 2D camera with
+/// an orthographic projection matrix to view the game world.
 fn spawn_camera(mut commands: Commands) {
-	commands.spawn(Camera2d);
+	commands.spawn((
+		PrimaryCamera,
+		Camera2d,
+		Projection::Orthographic(OrthographicProjection {
+			scale: 0.75,
+			..OrthographicProjection::default_2d()
+		}),
+	));
+}
+
+/// Moves the primary camera towards the camera target.
+///
+/// This system is run on the `Update` schedule and will use linear
+/// interpolation to move the transform of the camera towards the transform of
+/// the entity with the `CameraTarget` component.
+fn follow_camera_target(
+	target_transform: Single<&Transform, (With<CameraTarget>, Without<PrimaryCamera>)>,
+	mut camera_transform: Single<&mut Transform, (With<PrimaryCamera>, Without<CameraTarget>)>,
+	time: Res<Time>,
+) {
+	let Vec3 { x, y, .. } = target_transform.translation;
+	let direction = Vec3::new(x, y, camera_transform.translation.z);
+
+	camera_transform.translation = camera_transform
+		.translation
+		.lerp(direction, time.delta_secs() * CAMERA_LERP_FACTOR);
 }
--- src/player/spawn.rs
+++ src/player/spawn.rs
@@ -1,10 +1,12 @@
 use bevy::prelude::*;
 
+use crate::camera::CameraTarget;
 use crate::player::Player;
 
 pub(super) fn spawn_player(mut commands: Commands) {
 	commands.spawn((
 		Player,
+		CameraTarget,
 		Sprite::from_color(Color::WHITE, Vec2::new(32.0, 64.0)),
 	));
 }
